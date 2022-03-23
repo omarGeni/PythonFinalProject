5d0f6ecb52b779d679d90d96f11c59be036e614f
Omar <omkaydin@gmail.com>
2022-03-23 00:16:45 -0700

    Models

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Movie, MovieGenre, MovieRating
 
 # Register your models here.
+# Necessary if they are to appear in the admin
+admin.site.register(Movie)
+admin.site.register(MovieGenre)
+admin.site.register(MovieRating)
--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from  django.contrib.auth.models import User
 
 # Create your models here.
+# Each model will become a table in the database
+class MovieGenre(models.Model):
+    typename = models.CharField(max_length=255)
+    typedescription=models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.typename
+    
+    class Meta:
+        db_table='moviegenre'
+    
+class Movie(models.Model):
+    moviename=models.CharField(max_length=255)
+    moviecharacteristics=models.ForeignKey(MovieGenre, on_delete=models.CASCADE)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    entryDate=models.DateField()
+    movieUrl=models.URLField()
+    moviedescription=models.TextField()
+
+    def __str__(self):
+        return self.moviename
+    
+    class Meta:
+        db_table='Movie'
+
+class MovieRating(models.Model):
+    ratingscale=models.CharField(max_length=255)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    movie=models.ForeignKey(Movie, on_delete=models.CASCADE)
+    ratingdate=models.DateTimeField()
+    ratingText = models.TextField()
+    
+
+    def __str__(self):
+        return self.ratingscale
+
+    class Meta:
+        db_table='movierating'

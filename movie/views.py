f8b2b7a173454b6039eb921fa6da92096b4bf9d7
Omar <omkaydin@gmail.com>
2022-03-23 23:49:26 -0700

    Resource View

--- movie/views.py
+++ movie/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Movie, MovieGenre, MovieRating
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 
 # Create your views here.
 def index(request):
     return render(request, 'movie/index.html')
+
+def movies(request):
+    movie_list = Movie.objects.all()
+    return render(request, 'movie/movies.html', {'movie.list': movie_list})
